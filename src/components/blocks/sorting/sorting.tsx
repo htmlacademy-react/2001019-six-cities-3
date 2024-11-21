eb63c28a1e14e955de804519b7028b81d7e1d765
diana56178 <yusupova.diana.94@mail.ru>
2024-11-21 22:25:59 +0500

    реализует сортировку

--- src/components/blocks/sorting/sorting.tsx
+++ src/components/blocks/sorting/sorting.tsx
@@ -1,8 +1,21 @@
-import SortingItem from '../sorting-item/sorting-item.tsx';
-import {sortingOptions} from '../../../const.tsx';
+import {SortType} from '../../../const.tsx';
+import React, {Dispatch} from 'react';
 
+type TSorting = {
+    setSortType: Dispatch<string>;
+    currentSortType: string;
+};
 
-function Sorting(): JSX.Element {
+function Sorting({currentSortType, setSortType}: TSorting): JSX.Element {
+  const handleClick = (evt: React.MouseEvent) => {
+    const target = evt.target as HTMLElement;
+    const sortType = target.textContent;
+
+    if (sortType) {
+      setSortType(sortType);
+    }
+  };
+
   return (
     <form className="places__sorting" action="#" method="get">
       <span className="places__sorting-caption">Sort by</span>
@@ -12,10 +25,11 @@ function Sorting(): JSX.Element {
           <use xlinkHref="#icon-arrow-select"></use>
         </svg>
       </span>
-      <ul className="places__options places__options--custom places__options--opened">
-        {
-          sortingOptions.map((sortingItem) => <SortingItem option={sortingItem.name} key={sortingItem.id}/>)
-        }
+      <ul className="places__options places__options--custom places__options--opened" onClick={handleClick}>
+        <li className={currentSortType ? 'places__option places__option--active' : 'places__option'} tabIndex={0}>{SortType.POPULAR}</li>
+        <li className={currentSortType ? 'places__option places__option--active' : 'places__option'} tabIndex={0}>{SortType.CHEAP}</li>
+        <li className={currentSortType ? 'places__option places__option--active' : 'places__option'} tabIndex={0}>{SortType.EXPENSIVE}</li>
+        <li className={currentSortType ? 'places__option places__option--active' : 'places__option'} tabIndex={0}>{SortType.RATING}</li>
       </ul>
     </form>
   );
